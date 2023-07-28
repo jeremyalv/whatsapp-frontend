191e7a6cd25a2e48206727a69fd886fb424dd4a9
Jeremy Alva Prathama <jeremyalvax@gmail.com>
2023-07-28 23:36:43 +0700

    feat: improved chat bubble

--- components/ChatRoom/ChatBubble.tsx
+++ components/ChatRoom/ChatBubble.tsx
@@ -10,12 +10,27 @@ const ChatBubble = ({
   message,
   mine
 }: ChatBubbleProps) => {
+  const hours = new Date(message.created_at).getHours().toString().padStart(2, '0');
+  const minutes = new Date(message.created_at).getMinutes().toString().padStart(2, '0');
+  const chatTimestamp: string =  hours + ":" + minutes;
+  
   return (
-    <div className={`rounded-xl p-4 text-white 
-    ${mine
-      ? "place-self-end bg-teal-green-light rounded-tr-none" 
-      : "place-self-start bg-neutral-600 rounded-tl-none"}`}>
-      {message.content}
+    <div className={`relative flex flex-col ${mine ? "place-self-end" : "place-self-start"} group`}>
+      <div className={`flex flex-col rounded-xl px-4 py-3 text-white 
+      ${mine
+        ? "bg-teal-green-light rounded-tr-none" 
+        : "bg-neutral-600 rounded-tl-none"}
+        group-hover:cursor-pointer group-hover:bg-opacity-[0.75] group-hover:duration-100
+        `}>
+        <div>
+          {message.content}
+        </div>
+
+      </div>
+
+      <div className={`flex flex-row w-full ${mine ? "justify-end" : "justify-start"}`}>
+        {chatTimestamp}
+      </div>
     </div>
   )
 }
--- data/Messages.ts
+++ data/Messages.ts
@@ -3,6 +3,7 @@ export type MessageType = {
   content: string,
   room: string,
   user: string,
+  created_at: string,
 }
 
 export const Messages: MessageType[] = [
@@ -11,47 +12,55 @@ export const Messages: MessageType[] = [
     "content": "Hello World!",
     "room": "1",
     "user": "1",
+    "created_at": "2021-01-01T20:10:01.0007",
   },
   {
     "_id": "2",
     "content": "It's nice to meet everyone here!",
     "room": "1",
     "user": "2",
+    "created_at": "2021-01-01T20:10:01.0007",
   },
   {
     "_id": "3",
     "content": "Who's bringing pizza?",
     "room": "1",
     "user": "3",
+    "created_at": "2021-01-01T20:10:01.0007",
   },
   {
     "_id": "4",
     "content": "I think Josh is.",
     "room": "1",
     "user": "4",
+    "created_at": "2021-01-01T20:10:01.0007",
   },
   {
     "_id": "5",
     "content": "Sounds awesome!",
     "room": "1",
     "user": "1",
+    "created_at": "2021-01-01T20:10:01.0007",
   },
   {
     "_id": "6",
     "content": "First message!",
     "room": "2",
     "user": "1",
+    "created_at": "2021-01-01T20:10:01.0007",
   },
   {
     "_id": "7",
     "content": "Second Message message!",
     "room": "2",
     "user": "2",
+    "created_at": "2021-01-01T20:10:01.0007",
   },
   {
     "_id": "8",
     "content": "See you guys at 5.",
     "room": "1",
     "user": "1",
+    "created_at": "2021-01-01T20:10:01.0007",
   },
 ]
